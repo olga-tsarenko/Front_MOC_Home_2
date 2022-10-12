b9362f1179ccad6ef6cee9eea31203267ff9dc76
olga-tsarenko <olga.tsarenko7@gmail.com>
2022-10-12 20:42:15 +0300

    add close message

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 const formEl = document.getElementById('chat-form')
+const messagesList = document.querySelector('ul');
+const messageInput = formEl.querySelector('input');
+const myClose = document.getElementById('close_message')
+
 
 formEl.addEventListener("submit", (e) => {
+
     e.preventDefault();
 
-    const messageInput = formEl.querySelector('input');
-    let list = document.querySelector('ul');
-    let myListElem = document.createElement("li")
-
+    const myListElem = document.createElement("li")
     myListElem.classList.add('main-section-chat-list-item');
-    myListElem.innerHTML = messageInput.value;
-    list.appendChild(myListElem);
+    const myNewElem = (e) => {
+        myListElem.innerHTML = `<p> ${e.value} </p>` + `<span class="remove-button" id="remove-button"></span>`;
+    };
+
+    myNewElem(messageInput);
+
+    messagesList.appendChild(myListElem);
     messageInput.value = "";
 });
+
+
+const closeMyMessage = (event) => {
+    if (event.target.className !== "remove-button") return;
+
+    let myWrittenMessage = event.target.parentNode;
+
+    myWrittenMessage.remove();
+};
+
+messagesList.addEventListener('click', closeMyMessage, false);
