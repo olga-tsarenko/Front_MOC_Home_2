5916108b347d4f47b0e315d3bb75fe8e68f51779
olga-tsarenko <olga.tsarenko7@gmail.com>
2022-10-18 00:00:42 +0300

    add multiline_messages and scroll

--- js/main.js
+++ js/main.js
@@ -1,32 +1,50 @@
 const formEl = document.getElementById('chat-form')
 const messagesList = document.querySelector('ul');
-const messageInput = formEl.querySelector('input');
-const myClose = document.getElementById('close_message')
+const messageInput = formEl.querySelector('textarea');
 
+const newElemContent = text => (`<p> ${text.replace("\n", "<br>")} </p>` + `<span class="remove-button" id="remove-button"></span>`);
+const createNewMessage = () => {
+    const myListElem = document.createElement("li");
 
-formEl.addEventListener("submit", (e) => {
-
-    e.preventDefault();
-
-    const myListElem = document.createElement("li")
     myListElem.classList.add('main-section-chat-list-item');
-    const myNewElem = (e) => {
-        myListElem.innerHTML = `<p> ${e.value} </p>` + `<span class="remove-button" id="remove-button"></span>`;
-    };
-
-    myNewElem(messageInput);
-
+    myListElem.innerHTML = newElemContent(messageInput.value);
     messagesList.appendChild(myListElem);
     messageInput.value = "";
-});
 
-
+    myListElem.scrollIntoView();
+}
 const closeMyMessage = (event) => {
     if (event.target.className !== "remove-button") return;
 
     let myWrittenMessage = event.target.parentNode;
 
-    myWrittenMessage.remove();
-};
+    myWrittenMessage.animate(
+        {opacity: 0},
+        {duration: 1000}
+    )
+
+    setTimeout(() => myWrittenMessage.remove(), 1000);
+}
+
+const logKey = (e) => {
+    if (e.shiftKey && (e.keyCode === 13)) {
+        return;
+    }
+    if ((e.keyCode === 13)) {
+        e.preventDefault();
+
+        createNewMessage();
+    }
+}
+
+messageInput.addEventListener("keydown", logKey);
+
+formEl.addEventListener("submit", e => {
+    e.preventDefault();
+
+    messageInput.value && createNewMessage();
+});
 
 messagesList.addEventListener('click', closeMyMessage, false);
+
+
